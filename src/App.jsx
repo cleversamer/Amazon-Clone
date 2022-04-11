e0e56acb7b07b89b56b6107d07fa9e908a5c5ce8
Samer A <thedev.samer@gmail.com>
2022-04-11 12:39:21 +0300

    fixed some bugs & setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./css/app.css";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <h1>Let's build an Amazon Clone 🚀</h1>
+      <Routes>
+        <Route path="/checkout" element={<h2>Checkout Page</h2>} />
+        <Route path="/login" element={<h2>Login Page</h2>} />
+        <Route path="/home" element={<h2>Home Page</h2>} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<h2>Page Not Found</h2>} />
+      </Routes>
     </div>
   );
 };
