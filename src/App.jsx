6f182bb40bb26945461c04db7bd5061fdb052695
Samer A <thedev.samer@gmail.com>
2022-04-11 19:54:43 +0300

    added the not-found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 import "./css/app.css";
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
         <Route path="/checkout" element={<h1>Checkout Page</h1>} />
         <Route path="/login" element={<h1>Login Page</h1>} />
         <Route path="/home" element={<Home />} />
-        <Route path="/not-found" element={<h1>Page Not Found</h1>} />
+        <Route path="/not-found" element={<NotFound />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="*" element={<Navigate to="/not-found" replace />} />
       </Routes>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <img className="not-found__img" src="img/not-found.png" alt="Not found" />
+    </div>
+  );
+};
+
+export default NotFound;
